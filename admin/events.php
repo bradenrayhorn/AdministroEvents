b6de70a09687a49265021103d2a1de5df6ce74d3
bradenrayhorn <bradenrayhorn@users.noreply.github.com>
2017-05-26 09:21:16 -0600

    Initial commit

---
+++ admin/events.php
@@ -0,0 +1,77 @@
+<?php
+    // Load events
+    $administro->plugins['Events']->loadEvents();
+    $events = $administro->plugins['Events']->events;
+    // Generate nonce
+    $addEventNonce = $administro->generateNonce('addevent');
+?>
+<div class='title'>
+    Events
+</div>
+<div class='spacer'></div>
+<?php
+    echo $administro->plugins['Events']->eventDisplay(false);
+?>
+<div class='title sub'>
+    Add Event
+</div>
+<div>
+    <form method='post' action='<?php echo $administro->baseDir . 'form/addevent' ?>' enctype='multipart/form-data'>
+        <div class='row'>
+            <div class='two columns'>
+                <label>Name *</label>
+                <input type='text' name='name' required>
+            </div>
+            <div class='two columns'>
+                <label>Link</label>
+                <input type='url' name='link'>
+            </div>
+        </div>
+        <div class='row'>
+            <div class='one column'>
+                <label>Day *</label>
+                <select name='day'>
+                    <?php
+                        for($i = 1; $i <= 31; $i++) {
+                            echo '<option ' . sprintf("%04d", $i) . '>' . $i . '</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+            <div class='one column'>
+                <label>Month *</label>
+                <select name='month'>
+                    <option value='01'>January</option>
+                    <option value='02'>February</option>
+                    <option value='03'>March</option>
+                    <option value='04'>April</option>
+                    <option value='05'>May</option>
+                    <option value='06'>June</option>
+                    <option value='07'>July</option>
+                    <option value='08'>August</option>
+                    <option value='09'>September</option>
+                    <option value='10'>October</option>
+                    <option value='11'>November</option>
+                    <option value='12'>December</option>
+                </select>
+            </div>
+            <div class='one column'>
+                <label>Year *</label>
+                <select name='year'>
+                    <?php
+                        $year = date("Y");
+                        for($y = $year; $y < ($year + 5); $y++) {
+                            echo '<option value="' . $y . '">' . $y . '</option>';
+                        }
+                    ?>
+                </select>
+            </div>
+        </div>
+        <div class='row'>
+            <label>File</label>
+            <input type="file" name="file">
+        </div>
+        <input type='hidden' name='nonce' value='<?php echo $addEventNonce; ?>'>
+        <input class="button-primary" type="submit" value="Add Event">
+    </form>
+</div>
---
+++ plugin.php
@@ -0,0 +1,116 @@
+<?php
+
+    use Symfony\Component\Yaml\Yaml;
+
+    class EventsPlugin extends AdministroPlugin {
+
+        var $dataFile, $files, $events;
+
+        public function configLoaded() {
+            // Set file locations
+            $this->dataFile = $this->administro->rootDir . 'data/events/events.yaml';
+            $this->files = $this->administro->rootDir . 'data/events/files/';
+            // Create directory
+            @mkdir($this->files, 0777, true);
+            // Add event file viewer
+            $this->administro->reservedRoutes['eventfile'] = 'plugins/Events/eventroute.php';
+            // Add admin page
+            $this->administro->adminPages['events'] =
+                array('icon' => 'calendar', 'name' => 'Events', 'file' => 'plugins/Events/admin/events.php');
+            // Add variable
+            $this->administro->variables['events'] = $this->eventDisplay();
+            // Add forms
+            array_push($this->administro->forms, 'addevent');
+        }
+
+        public function eventDisplay($hideOld = true) {
+            $this->loadEvents();
+
+            uasort($this->events, function($p1, $p2) {
+                $c1 = new DateTime($p1['date']);
+                $c2 = new DateTime($p2['date']);
+                if($c1 < $c2) return -1;
+                if($c1 == $c2) return 0;
+                if($c1 > $c2) return 1;
+            });
+
+            $html = '';
+
+            foreach($this->events as $id => $event) {
+                $date = new DateTime($event['date']);
+                if($hideOld && new DateTime() > $date) continue;
+                $month = $date->format('F');
+                $day = $date->format('j');
+                $name = $event['name'];
+                $link = false;
+                if($event['file'] !== false) {
+                    $link = $this->administro->baseDir . 'eventfile/' . $event['file'];
+                } else if($event['link'] !== false) {
+                    $link = $event['link'];
+                }
+                if($link !== false) {
+                    $html .= '<div class="event"><a href="' . $link . '"><span class="date">'
+                        . $month . ' ' . $day . ': </span>' . $name . '</a></div>';
+                } else {
+                    $html .= '<div class="event"><span class="date">' . $month . ' ' . $day . ': </span>' . $name . '</div>';
+                }
+            }
+
+            return $html;
+        }
+
+        public function loadEvents() {
+            // Make sure file exists
+            if(!file_exists($this->dataFile)) {
+                file_put_contents($this->dataFile, Yaml::dump(array()));
+            }
+            // Load events
+            $this->events = Yaml::parse(file_get_contents($this->dataFile));
+        }
+
+    }
+
+    function addeventform($administro) {
+        $params = $administro->verifyParameters('addevent', array('name', 'day', 'month', 'year'));
+        if($params !== false) {
+            // Verify permission
+            if($administro->hasPermission('admin.speakers')) {
+                // Load events
+                $administro->plugins['Events']->loadEvents();
+                $events = $administro->plugins['Events']->events;
+                // Generate event id
+                $id = uniqid();
+                while(isset($events[$id])) {
+                    $id = uniqid();
+                }
+                // Generate the event
+                $event = array(
+                    'name' => $params['name'],
+                    'date' => $params['year'] . '-' . $params['month'] . '-' . $params['day'],
+                    'link' => false,
+                    'file' => false
+                );
+                // Check if event has link or file
+                if(isset($_FILES['file']) && !empty($_FILES['file']['name'])) {
+                    $file = $id . '.' . pathinfo($_FILES['file']['name'])['extension'];
+                    $event['file'] = $file;
+                    // Save the file
+                    if ($_FILES['file']['size'] > 10000000) {
+                        $administro->redirect('admin/events', 'bad/File must be under 10MB!');
+                    }
+                    move_uploaded_file($_FILES['file']['tmp_name'],
+                        $administro->rootDir . 'data/events/files/' . $file);
+                } else if(isset($_POST['link']) && !empty($_POST['link'])) {
+                    $event['link'] = $_POST['link'];
+                }
+                // Write the event
+                $events[$id] = $event;
+                file_put_contents($administro->plugins['Events']->dataFile, Yaml::dump($events));
+                $administro->redirect('admin/events', 'good/Added event!');
+            } else {
+                $administro->redirect('admin/home', 'bad/Invalid permission!');
+            }
+        } else {
+            $administro->redirect('admin/events', 'bad/Invalid parameters!');
+        }
+    }
